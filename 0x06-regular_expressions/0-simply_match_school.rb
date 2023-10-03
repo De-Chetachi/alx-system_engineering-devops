cde4e3d2de3a52ef444ba8b3a62e844a77c3f90e
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-10-03 21:42:36 +0100

    alot

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,6 @@
 #!/usr/bin/env ruby
 #The regular expression must match School
-/School/
+if ARGV.length == 1
+  puts ARGV[0].scan(/School/).join("")
+  exit
+end
---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+#The regular expression must be only matching: capital letters
+
+if ARGV.length == 1
+  puts ARGV[0].scan(/[A-Z]/).join("")
+  exit
+end
