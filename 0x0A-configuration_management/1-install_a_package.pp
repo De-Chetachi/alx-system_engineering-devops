646b3090deb0219670abb85436aca9ef1e064fde
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-11-24 23:13:47 +0100

    devops

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,17 @@
 #Using Puppet, create a file in /tmp.
 
 package { 'python3-pip':
-  ensure => 'installed'
+  ensure => 'installed',
 }
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip']
+  #provider => 'pip3',
+  require  => Package['python3-pip'],
+  provider => 'pip3'
+}
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  #provider => 'pip3',
+  require  => Package['python3-pip'],
+  provider => 'pip3'
 }
