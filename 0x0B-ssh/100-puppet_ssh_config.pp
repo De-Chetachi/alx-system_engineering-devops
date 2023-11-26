c661eba3c8026175af69654acb63a57678c4faff
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-11-26 22:02:16 +0100

    puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,5 @@
+#a manifest that kills a process named killmenow.
+
+exec { 'echo':
+  command => '/usr/bin/echo "	IdentityFile ~/.ssh/school\n	PasswordAuthentication no" >> /etc/ssh/ssh_config',
+}
