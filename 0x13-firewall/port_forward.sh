4e5a717b14158214d34bb138f01f0322a1ae2ad9
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-12-26 15:31:34 +0100

    adv

--- 0x13-firewall/port_forward.sh
+++ 0x13-firewall/port_forward.sh
@@ -2,7 +2,9 @@
 #configure port forwarding
 
 sudo su
+ufw route allow 8080
 echo "net/ipv4/ip_forward=1" >> /etc/ufw/sysctl.conf
+sysctl -p
 echo "*nat
 :PREROUTING ACCEPT [0:0]
 -A PREROUTING -p tcp --dport 8080 -j REDIRECT --to-port 80
