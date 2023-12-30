67ed725266d5b2b5c263f33f6daae79f335da594
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-12-30 13:56:40 +0100

    ppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,21 +4,6 @@ package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'red':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-}
-
-file { '/var/www/html/custom_404.html':
-  content => "Ceci n'est pas une page",
-}
-
 exec { 'hostname':
   command => '/usr/bin/hostname',
   logoutput => true,
