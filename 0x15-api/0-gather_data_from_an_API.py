e969d1c2c5254a906dfc6e76e562054ee5a6a92c
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-01-29 23:06:23 +0100

    alpha ordered imports

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 '''using this REST API, for a given employee ID, returns
 information about his/her TODO list progress.'''
+import json
 import requests
 import sys
-import json
 
 
 def get_todo():
