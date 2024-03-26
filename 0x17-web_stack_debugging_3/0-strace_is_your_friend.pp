88b4b46116941535bd08adf9596693f87146bc7e
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-03-26 22:26:14 +0100

    ok

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,13 @@
 # configure the  wp-settings.php file to properly serve wordpress pages
 
 file { '/var/www/html/wp-settings.php':
-  ensure  => present,                                                                                                         content => inline_template('<%= File.read("/var/www/html/wp-settings.php").gsub("class-wp-locale.phpp", "class-wp-locale.php") %>'),
-}                                                                                                                                                                                                                                                       exec { 'restartApache':
+  ensure  => present,
+  content => inline_template('<%= File.read("/var/www/html/wp-settings.php").gsub("class-wp-locale.phpp", "class-wp-locale.php") %>'),
+}
+
+exec { 'restartApache':
   command     => 'initctl restart apache2',
-  path        => '/sbin:/usr/sbin:/bin',                                                                                      refreshonly => true,
-  subscribe   => File['/var/www/html/wp-settings.php'],                                                                     }         
+  path	=> '/sbin:/usr/sbin:/bin',
+  refreshonly => true,
+  subscribe   => File['/var/www/html/wp-settings.php'],
+}
