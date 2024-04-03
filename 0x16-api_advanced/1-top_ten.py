37df2831013ac139f1588db114a3321f27be73b7
Chetachi Onyenze <124177018+De-Chetachi@users.noreply.github.com>
2024-04-04 00:10:27 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
-'''this module contains a function that queries the reddit api'''
+"""
+Module Docs
+"""
 import requests
 
 
 def top_ten(subreddit):
-    '''this function queries the reddit api
-    arg: subreddit
-    prints the titles of the first 10 hot posts listed in the given subreddit
-    if subreddit is invalid print None'''
-    # Mozilla/5.0 (Windows NT 10.0; Win64; x64)
-    # AppleWebKit/537.36 (KHTML, like Gecko)
-    # Chrome/123.0.0.0 Safari/537.36
-
+    """
+    Function Docs
+    """
+    url = 'https://www.reddit.com'
     header = {
-            'Accept': 'application/json',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-            }
-    base_url = 'https://www.reddit.com'
-    sr_url = '{}/r/{}/.json?sort={}&limit=9'.format(base_url, subreddit, 'top')
-
-    sr = requests.get(sr_url, headers=header, allow_redirects=False)
-    if sr.status_code == 200:
-        sr_data = sr.json()['data']['children']
-        for child in sr_data:
-            print(child['data']['title'])
+        'Accept': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    }
+    response = requests.get('{}/r/{}/.json?sort={}&limit={}'.format(
+        url, subreddit, 'top', 10),
+        headers=header,
+        allow_redirects=False)
+    if response.status_code == 200:
+        for post in response.json()['data']['children'][0:10]:
+            print(post['data']['title'])
     else:
-        print('None')
+        print(None)
